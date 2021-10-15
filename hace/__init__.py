3c5041ba9b150c6f1d94cc43a116d748a7d5c63c
Yannick Uhlmann <augustunderground@protonmail.com>
2021-10-15 14:20:51 +0200

    wrong exports

--- hace/__init__.py
+++ hace/__init__.py
@@ -1,15 +1,18 @@
 import hy
 
-from .hace import single_ended_opamp     \
-                , set_parameters         \
-                , evaluate_circuit       \
-                , current_performance    \
-                , performance_parameters \
-                , random_sizing          \
-                , current_sizing         \
-                , initial_sizing         \
-                , simulation_analyses    \
+from .hace import single_ended_opamp      \
+                , set_parameter           \
+                , set_parameters          \
+                , evaluate_circuit        \
+                , simulation_analyses     \
+                , current_performance     \
+                , performance_identifiers \
+                , random_sizing           \
+                , initial_sizing          \
+                , current_parameters      \
                 , parameter_identifiers
 
-from .util import __name__  \
-                , __version__
+from .util import __name__      \
+                , __version__   \
+                , jsa_to_list   \
+                , jmap_to_dict
